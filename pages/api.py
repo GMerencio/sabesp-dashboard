026ace49437be4ce902a236e00b0441b594aecbe
GMerencio <gmerencio.santos@gmail.com>
2023-10-27 19:39:18 -0300

    Páginas de dados e API finalizadas

--- pages/api.py
+++ pages/api.py
@@ -4,4 +4,4 @@ from st_pages import add_page_title
 
 add_page_title()
 
-st.markdown('''Em construção.''')
+st.markdown('''Desenvolvedores podem acessar a [API REST](https://sabesp-api.onrender.com/) que disponibiliza os dados de sistemas e reservatórios, atualizados diariamente. [O repositório do projeto pode ser acessado aqui](https://github.com/GMerencio/sabesp-api).''')
